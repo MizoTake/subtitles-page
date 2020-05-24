e09e73eb72bcc186502e5e7501eb387df4838530
MizoTake <mizotake123@gmail.com>
2020-05-24 11:44:11 +0900

    speech to textのベース

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="viewText">
+    <v-textarea cols="12" md="6"
+      filled
+      name=""
+      v-model="currentText"
+      v-bind:readonly="true"
+  ></v-textarea>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    speechRecognition: window.SpeechRecognition || window.webkitSpeechRecognition,
+    currentText: '',
+    stackText: []
+  }),
+  methods: {
+    settingRecognition () {
+      var recognition = new this.speechRecognition()
+      recognition.lang = 'ja-JP'
+      recognition.interimResults = true
+      recognition.continuous = true
+
+      recognition.addEventListener('result', event => {
+        this.stackText = Array.from(event.results).map(x => x[0]).map(x => x.transcript)
+        this.currentText = this.stackText.join('。')
+      })
+
+      recognition.addEventListener('end', () => {
+        recognition.start()
+      })
+
+      recognition.start()
+    }
+  },
+  mounted () {
+    if (!this.speechRecognition) {
+      alert('Chromeをお使いください。')
+      return
+    }
+    this.settingRecognition()
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.viewText {
+  text-align: center
 }
+
 </style>
