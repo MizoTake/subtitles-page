670cdd8d3e1e1d8996acdf6d46106e8215b8cdc2
MizoTake <mizotake123@gmail.com>
2020-05-24 13:47:55 +0900

    設定画面との結びつき

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,63 @@
 <template>
-  <div class="viewText">
-    <v-textarea cols="12" md="6"
-      filled
-      name=""
-      v-model="currentText"
-      v-bind:readonly="true"
-  ></v-textarea>
+  <div id="app" v-on:click="onClick"
+    v-bind:style="{ width: windowWidth + 'px', height: windowHeight + 'px' }">
+    <ul class="subtitles">
+      <li v-for="(str, index) in sliceText" :key="index">
+        {{ str }}
+      </li>
+    </ul>
+    <overlaySettings ref="overlaySettings" 
+    v-bind="{
+      lineStrValue: lineStrValue
+    }"
+    v-on:disableOverlay="disableOverlayFromoverlaySettings"
+    v-on:updateLineStrValue="updateLineStrValue" />
   </div>
 </template>
 
 <script>
+import overlaySettings from "./components/OverlaySettings.vue"
+
 export default {
   name: 'App',
   data: () => ({
     speechRecognition: window.SpeechRecognition || window.webkitSpeechRecognition,
+    windowWidth: window.innerWidth,
+    windowHeight: window.innerHeight,
     currentText: '',
-    stackText: []
+    overlay: false,
+    overlaySettingsDisable: false,
+    lineStrValue: 10
   }),
+  computed: {
+   sliceText: function () {
+      const result = []
+      for (let i = 0; i < this.currentText.length; i += this.lineStrValue) {
+        result.push(this.currentText.slice(i, i + this.lineStrValue))
+      }
+      return result
+    }
+  },
   methods: {
+    handleResize: function() {
+      this.windowWidth = window.innerWidth
+      this.windowHeight = window.innerHeight
+    },
+    onClick: function() {
+      if (this.overlaySettingsDisable) {
+        this.overlaySettingsDisable = false
+        return
+      }
+      this.overlay = true
+      this.$refs.overlaySettings.enableOverlay()
+    },
+    disableOverlayFromoverlaySettings: function() {
+      this.overlay = false
+      this.overlaySettingsDisable = true
+    },
+    updateLineStrValue: function(value) {
+      this.lineStrValue = parseInt(value)
+    },
     settingRecognition () {
       var recognition = new this.speechRecognition()
       recognition.lang = 'ja-JP'
@@ -25,12 +65,16 @@ export default {
       recognition.continuous = true
 
       recognition.addEventListener('result', event => {
-        this.stackText = Array.from(event.results).map(x => x[0]).map(x => x.transcript)
-        this.currentText = this.stackText.join('。')
+        const stackText = Array.from(event.results).map(x => x[0]).map(x => x.transcript)
+        this.currentText = stackText.join('。')
+        this.lineStrArray = []
+        for (let i = 0; i < this.currentText.length; i += this.lineStrValue) {
+          this.lineStrArray.push(this.currentText.slice(i, i + this.lineStrValue))
+        }
       })
 
-      recognition.addEventListener('end', () => {
-        recognition.start()
+      recognition.addEventListener('speechend', () => {
+        // recognition.start()
       })
 
       recognition.start()
@@ -42,14 +86,24 @@ export default {
       return
     }
     this.settingRecognition()
-  }
+    window.addEventListener("resize", this.handleResize)
+  },
+  beforeDestroy: function() {
+    window.removeEventListener("resize", this.handleResize)
+  },
+  components: { overlaySettings }
 }
 </script>
 
 <style>
-
-.viewText {
-  text-align: center
+.subtitles {
+	text-align: center;
+}
+.subtitles-block {
+	text-align: center;
+}
+.subtitles-block li {
+	display: inline-block;
 }
 
 </style>
---
+++ src/components/OverlaySettings.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="screen">
+    <div id="overlay" v-if="overlay" v-on:click.self="disableOverlay">
+      <ul id="example-1">
+         <v-col cols="12" sm="6" md="3">
+          <v-text-field
+            label="一行あたりの文字数"
+            v-bind:value="lineStrValue"
+            @change="updateLineStrValue"
+          ></v-text-field>
+        </v-col>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['lineStrValue'],
+  data: () => ({
+    overlay: false,
+  }),
+  methods: {
+    enableOverlay: function() {
+      this.overlay = true;
+    },
+    disableOverlay: function() {
+      this.overlay = false;
+      this.$emit("disableOverlay")
+    },
+    updateLineStrValue: function(value) {
+      this.$emit("updateLineStrValue", value)
+    }
+  }
+};
+</script>
+
+<style>
+.screen * {
+  border: 0;
+  margin: 0;
+  padding: 0;
+}
+.screen {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+}
+
+#overlay {
+  z-index: 1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(109, 109, 109, 0.5);
+  align-items: center;
+  justify-content: center;
+}
+</style>
